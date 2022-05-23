2e3448b64d5b51284014b2746590405f823dced3
Dan Seminara <japaneseforgaming@gmail.com>
2022-05-23 15:00:51 -0400

    In progress

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+
   def index
     items = Item.all
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+  
+  def create
+    user = User.find(params[:user_id])
+
+      item = user.items.create(permitted)
+      render json: item, status: :created
+
+  end
+
+
+  private
+
+  def permitted
+    params.permit(:name, :description, :price)
+  end
+
+  def not_found_response
+    render json:{error: "Not found"}, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find_by!(id: params[:id])
     render json: user, include: :items
   end
+
+  private
+
+
+  def not_found_response
+    render json:{error: "Not found"}, status: :not_found
+  end
+
 
 end
